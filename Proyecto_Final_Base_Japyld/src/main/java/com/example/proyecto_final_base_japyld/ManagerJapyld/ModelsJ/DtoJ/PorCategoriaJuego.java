36fed376b0e8eae0c2af5ed6f1f1527b712d2f61
milenelarosa <134633068+milenelarosa@users.noreply.github.com>
2023-06-15 01:30:54 -0500

    Manager: Modulo de Juegos y Cruds Terminado

---
+++ Proyecto_Final_Base_Japyld/src/main/java/com/example/proyecto_final_base_japyld/ManagerJapyld/ModelsJ/DtoJ/PorCategoriaJuego.java
@@ -0,0 +1,22 @@
+package com.example.proyecto_final_base_japyld.ManagerJapyld.ModelsJ.DtoJ;
+
+public class PorCategoriaJuego {
+    private String nombreCategoria;
+    private int num_usuarios;
+
+    public String getNombreCategoria() {
+        return nombreCategoria;
+    }
+
+    public void setNombreCategoria(String nombreCategoria) {
+        this.nombreCategoria = nombreCategoria;
+    }
+
+    public int getNum_usuarios() {
+        return num_usuarios;
+    }
+
+    public void setNum_usuarios(int num_usuarios) {
+        this.num_usuarios = num_usuarios;
+    }
+}
---
+++ Proyecto_Final_Base_Japyld/src/main/java/com/example/proyecto_final_base_japyld/ManagerJapyld/ModelsJ/DtoJ/ProductosVendidos.java
@@ -0,0 +1,53 @@
+package com.example.proyecto_final_base_japyld.ManagerJapyld.ModelsJ.DtoJ;
+
+public class ProductosVendidos {
+    private int id_juego;
+
+    private int ventas;
+
+    private String direccion_archivo;
+
+    private String nombreJuego;
+
+    private int precio;
+
+    public int getId_juego() {
+        return id_juego;
+    }
+
+    public void setId_juego(int id_juego) {
+        this.id_juego = id_juego;
+    }
+
+    public int getVentas() {
+        return ventas;
+    }
+
+    public void setVentas(int ventas) {
+        this.ventas = ventas;
+    }
+
+    public String getDireccion_archivo() {
+        return direccion_archivo;
+    }
+
+    public void setDireccion_archivo(String direccion_archivo) {
+        this.direccion_archivo = direccion_archivo;
+    }
+
+    public String getNombreJuego() {
+        return nombreJuego;
+    }
+
+    public void setNombreJuego(String nombreJuego) {
+        this.nombreJuego = nombreJuego;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+}
