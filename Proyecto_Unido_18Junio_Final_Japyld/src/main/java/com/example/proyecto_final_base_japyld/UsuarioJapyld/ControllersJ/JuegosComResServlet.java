80aa487bf9c41e8eda1d0c7ba7dbea7089e2a4f6
Yosthim <a20201497@pucp.edu.pe>
2023-07-13 22:58:23 -0500

    Página de juegos comprados y reservados terminado

--- Proyecto_Unido_18Junio_Final_Japyld/src/main/java/com/example/proyecto_final_base_japyld/UsuarioJapyld/ControllersJ/JuegosComResServlet.java
+++ Proyecto_Unido_18Junio_Final_Japyld/src/main/java/com/example/proyecto_final_base_japyld/UsuarioJapyld/ControllersJ/JuegosComResServlet.java
@@ -1,5 +1,7 @@
 package com.example.proyecto_final_base_japyld.UsuarioJapyld.ControllersJ;
 
+import com.example.proyecto_final_base_japyld.BeansGenerales.Personas;
+import com.example.proyecto_final_base_japyld.UsuarioJapyld.ModelsJ.DaosJ.TusJuegosDao;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -15,5 +17,21 @@ public class JuegosComResServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         RequestDispatcher view;
+
+        TusJuegosDao tusJuegosDao = new TusJuegosDao();
+        Personas usuario = (Personas) request.getSession().getAttribute("personaSession");
+
+        String categoria = tusJuegosDao.categoriaMasComprada(usuario.getIdPersona());
+        //En caso el usuario no haya comprado retornará nulo, entonces obtenemos su categoria preferida (pedida en el registro)
+        if (categoria == null) {
+            categoria = tusJuegosDao.categoriaPreferida(usuario.getIdPersona());
+        }
+
+        //Asignamos los valores correspondientes como atributos de la página
+        request.setAttribute("categoriaPreferida", categoria);
+        request.setAttribute("listaJuegos", tusJuegosDao.listarJuegos(usuario.getIdPersona()));
+        request.setAttribute("juegosRecomendados", tusJuegosDao.listarJuegosRecomendados(categoria));
+        view = request.getRequestDispatcher("UsuarioJapyld/TusJuegos.jsp");
+        view.forward(request, response);
     }
 }
