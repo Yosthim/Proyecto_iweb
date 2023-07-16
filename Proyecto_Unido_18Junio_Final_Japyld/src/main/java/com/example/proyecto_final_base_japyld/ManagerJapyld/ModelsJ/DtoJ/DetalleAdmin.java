80cc37928d3af398d23d341ba20489979a73ff5c
LuisAngel <a20192832@pucp.edu.pe>
2023-07-16 17:02:31 -0500

    ImagenesListasxd

--- Proyecto_Unido_18Junio_Final_Japyld/src/main/java/com/example/proyecto_final_base_japyld/ManagerJapyld/ModelsJ/DtoJ/DetalleAdmin.java
+++ Proyecto_Unido_18Junio_Final_Japyld/src/main/java/com/example/proyecto_final_base_japyld/ManagerJapyld/ModelsJ/DtoJ/DetalleAdmin.java
@@ -23,6 +23,8 @@ public class DetalleAdmin {
     private BigDecimal precioCompra;
     private BigDecimal DineroTotal;
 
+    private String direccion_imagen;
+
     public int getId() {
         return id;
     }
@@ -141,5 +143,13 @@ public class DetalleAdmin {
 
     public void setDineroTotal(BigDecimal dineroTotal) {
         DineroTotal = dineroTotal;
+    }
+
+    public String getDireccion_imagen() {
+        return direccion_imagen;
+    }
+
+    public void setDireccion_imagen(String direccion_imagen) {
+        this.direccion_imagen = direccion_imagen;
     }
 }
